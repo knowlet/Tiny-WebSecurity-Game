bd75031985c8fdc0ccd43237314c7e52c0914785
knowlet <knowlet3389@gmail.com>
2015-06-10 11:30:10 +0800

    fix hint

--- file.php
+++ file.php
@@ -3,6 +3,7 @@
 if(isset($_GET['name']))
 {
     // the key is not here (troll)
+    // maybe version controll?
     header("Content-type:application");
     header("Content-Length: " .(string)(filesize($_GET['name'])));
     header("Content-Disposition: attachment; filename=".$_GET['name']);
